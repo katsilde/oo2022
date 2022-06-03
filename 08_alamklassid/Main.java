ed52d21420f3f758f1dce34d3cca102e93881053
Katrin Silde <90237367+katsilde@users.noreply.github.com>
2022-06-03 10:58:09 +0300

    Create Main.java

---
+++ 08_alamklassid/Main.java
@@ -0,0 +1,12 @@
+package kodutoo;
+
+public class Main {
+    public static void main(String[] args) {
+        Traktor traktor = new Traktor();
+        traktor.Kynna();
+        traktor.Vea();
+          
+        Vikat vikat = new Vikat();
+        vikat.Niida();
+    }
+}
