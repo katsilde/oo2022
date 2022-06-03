119c6e92bca0a6664ba50836d3023df6a0ba21fa
Katrin Silde <90237367+katsilde@users.noreply.github.com>
2022-06-03 11:12:01 +0300

    Update Main.java

--- 08_alamklassid/Main.java
+++ 08_alamklassid/Main.java
@@ -3,9 +3,12 @@ package kodutoo;
 public class Main {
     public static void main(String[] args) {
         Traktor traktor = new Traktor();
+        traktor.Teen_tood();
         traktor.Kynna();
+        
           
         Vikat vikat = new Vikat();
+        vikat.Abista();
         vikat.Niida();
     }
 }
