3655917fc2d6b8450743a142f76eb706a71a2da3
Katrin Silde <90237367+katsilde@users.noreply.github.com>
2022-06-03 11:07:55 +0300

    Update Main.java

--- 08_alamklassid/Main.java
+++ 08_alamklassid/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         Traktor traktor = new Traktor();
         traktor.Kynna();
-        traktor.Vea();
           
         Vikat vikat = new Vikat();
         vikat.Niida();
