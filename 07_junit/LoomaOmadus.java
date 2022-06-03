d79d8e887220608d50c980034ec98298cc0b5e91
Katrin Silde <90237367+katsilde@users.noreply.github.com>
2022-06-03 11:44:48 +0300

    Create LoomaOmadus.java

---
+++ 07_junit/LoomaOmadus.java
@@ -0,0 +1,15 @@
+public interface LoomaOmadus {
+
+    String getliik();
+    String getnimi();
+    String getvarv();
+    int getvanus();
+    String getkiibinumber();
+    int getviimanevaktsiin();
+
+
+    String vaktsineerimine();
+    String kasteebmjau();
+    String kiibistatud();
+
+}
